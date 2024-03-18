499ba67a656ad14a23ca6a0ed0a25ae9b88a71bf
samannnn2 hlb <saman.hallajbashi@gmail.com>
2024-03-18 12:06:17 +0000

    feat: inserting data to DB

--- number_guess.sh
+++ number_guess.sh
@@ -6,7 +6,7 @@ NUMBER=$((1 + RANDOM%1000))
 echo $NUMBER
 
 echo "Enter your username:"
-read USERNAME
+read USER_NAME
 
 USER_ID=$($PSQL "
 SELECT user_id
@@ -16,14 +16,31 @@ WHERE name = '$USER_NAME'
 
 if [[ -z $USER_ID ]]
 then
-
   INSERT_NEW_USER=$($PSQL "
   INSERT
   INTO users(name)
-  VALUES('$USER_NAME')
+  VALUES('$USER_NAME');
   ")
 
   echo "Welcome, $USER_NAME! It looks like this is your first time here."
 else
-  echo "Welcome back, $USER_NAME! You have played <games_played> games, and your best game took <best_game> guesses."
+  USER_NAME=$($PSQL "
+  SELECT name
+  FROM users
+  WHERE user_id = '$USER_ID'
+  ")
+
+  USER_GAMES=$($PSQL "
+  SELECT games_played
+  FROM users
+  WHERE user_id = '$USER_ID'
+  ")
+
+  USER_RECORD=$($PSQL "
+  SELECT best_game
+  FROM users
+  WHERE user_id = '$USER_ID'
+  ")
+
+  echo "Welcome back, $USER_NAME! You have played $USER_GAMES games, and your best game took $USER_RECORD guesses."
 fi
